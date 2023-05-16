1578b24ed25bcdbdf6804711d7baad671f70839d
meleadr <meledina.adriano@gmail.com>
2023-05-16 20:54:07 +0200

    Affichage des différentes catégories

--- main.py
+++ main.py
@@ -22,12 +22,8 @@ def main():
     translator = Translator(language)
     name = input(translator.translate("what-name"))
     print(translator.translate("hello"), name)
-
-    url = (
-        "https://api.themoviedb.org/3/movie/now_playing?language="
-        + language
-        + "&page=1"
-    )
+    lg = language.split("-")[0]
+    url = "https://api.themoviedb.org/3/genre/movie/list?language=en"
 
     headers = {
         "accept": "application/json",
@@ -35,6 +31,19 @@ def main():
     }
 
     response = requests.get(url, headers=headers)
+
+    # url = (
+    #     "https://api.themoviedb.org/3/movie/now_playing?language="
+    #     + language
+    #     + "&page=1"
+    # )
+
+    # headers = {
+    #     "accept": "application/json",
+    #     "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYTJkMGQ0YmVjOWQ4NjFhMjFjMmJlMzAwZGI5NDk2NiIsInN1YiI6IjY0NjNjNmI2OGM0NGI5NzgwNzFmMzVhNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LrT3myEXOkzgpq6nlHWLCEV0aOEsjfW0Bs342-Lu1ZQ",
+    # }
+
+    # response = requests.get(url, headers=headers)
 
     print(response.text)
 
